ef9d057513e7256e866a359220dcd55d8c648f41
elise <Elisenovaes10@gmail.com>
2024-06-24 13:43:11 -0300

    Criação de Dashboard
    Criação de telas de Dashboard

---
+++ client/src/Pages/Dashboard/index.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import BottomNavigation from './BottomNavigation';
+import '.style.css';
+
+const Dashboard = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Preço Certo</h1>
+      <div className="topproducts">
+        <h2 className="toptitle">Top Produtos</h2>
+        <div className="products">
+          <div className="product">Produto 1<br/><span className='sales'> XX Vendas</span></div>
+          <div className="product">Produto 2<br/><span className='sales'> XX Vendas</span></div>
+          <div className="product">Produto 3<br/><span className='sales'> XX Vendas</span></div>
+          <div className="product">Produto 4<br/><span className='sales'> XX Vendas</span></div>
+        </div>
+      </div>
+      <button className='buttonL'>Precificações</button>
+      <button className='buttonL'>Balanço</button>
+      <BottomNavigation/>
+    </div>
+
+  );
+};
+
+export default Dashboard;
