784ef904a778cd6a0b779d9fe6dcb65297adbc85
elise <Elisenovaes10@gmail.com>
2024-06-24 14:45:04 -0300

    Adição de Rotas

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,16 +5,20 @@ import RegisterScreen from './Pages/Register';
 import ResetPasswordScreen from './Pages/ResetPass';
 import ProfileEdit from './Pages/ProfileEdit';
 import UserProfile from './Pages/UserProfile';
+import Dashboard from './Pages/Dashboard';
+import BillsRegister from './Pages/RegisterBills';
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<LoginScreen/>} />
+        <Route path="/Dashboard" element={<Dashboard/>}/>
         <Route path="/Register" element={<RegisterScreen/>} />
         <Route path="/ResetPassword" element={<ResetPasswordScreen/>} />
         <Route path="/ProfileEdit" element={<ProfileEdit/>} />
         <Route path="/UserProfile" element={<UserProfile/>} />
+        <Route path="/BillsRegister" element={<BillsRegister/>} />
       </Routes>
     </Router>
   );
