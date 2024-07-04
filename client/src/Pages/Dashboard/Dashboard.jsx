e5fe303591f8c662d2dca506728651591b58ef31
João Lyra <joaolyra24@gmail.com>
2024-07-04 08:15:21 -0300

    Update Dashboard

--- client/src/Pages/Dashboard/Dashboard.jsx
+++ client/src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
-
-import '../../Components/Style.css';
+import './Dashboard.css';
+import LogoPC from '../../Assets/LogoPC.png'
 import { useNavigate } from 'react-router-dom';
 import BottomNavigation from '../../Components/BottomNavigation/BottomNavigation';
 
@@ -12,19 +12,29 @@ const Dashboard = () => {
     navigate('/RegisterBills');
 };
   return (
-    <div className="container">
-      <h1 className="title">Preço Certo</h1>
-      <div className="topproducts">
-        <h2 className="toptitle">Top Produtos</h2>
-        <div className="products">
-          <div className="product">Produto 1<br/><span className='sales'> XX Vendas</span></div>
-          <div className="product">Produto 2<br/><span className='sales'> XX Vendas</span></div>
-          <div className="product">Produto 3<br/><span className='sales'> XX Vendas</span></div>
-          <div className="product">Produto 4<br/><span className='sales'> XX Vendas</span></div>
+    <div className='t3'>
+      <div className='t3-header'>
+        <h1 >Preço Certo</h1>
+        <img src={LogoPC} alt="Preço Certo"/>
+      </div>
+      <div className='t3-bottom'>
+        <h1 className='t3-title'>Top Produtos</h1>
+        <div className='t3-resume'>
+          <div className='t3-resume-body'>
+            <div className="t3-product">Produto 1<br/><span className='t3-sales'> XX Vendas</span></div>
+            <div className="t3-product">Produto 2<br/><span className='t3-sales'> XX Vendas</span></div>
+          </div>
+          <div className='t3-resume-body'>
+            <div className="t3-product">Produto 3<br/><span className='t3-sales'> XX Vendas</span></div>
+            <div className="t3-product">Produto 4<br/><span className='t3-sales'> XX Vendas</span></div>
+          </div>
+        </div>
+      
+        <div className='t3-box-button'>
+          <button className='t3-button'>Precificações</button>
+          <button onClick={handleBills} className='t3-button'>Balanço</button>
         </div>
       </div>
-      <button className='buttonL'>Precificações</button>
-      <button onClick={handleBills} className='buttonL'>Balanço</button>
       <BottomNavigation/>
     </div>
 
