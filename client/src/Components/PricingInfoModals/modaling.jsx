b29482fa0631c30b1eea069d93b29dc9b2dc54a6
elise <Elisenovaes10@gmail.com>
2024-11-01 15:59:25 -0300

    Update modaling.jsx

--- client/src/Components/PricingInfoModals/modaling.jsx
+++ client/src/Components/PricingInfoModals/modaling.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import React from 'react';
 import { useIngredient } from '../../Hooks/useIngredient';
 
 const ModalIng = ({ onAddIngredient, onClose, ingredientToEdit }) => {
