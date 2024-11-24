102c4dd8db6067f01ac4c3a738628a0ba88d2cb9
elise <Elisenovaes10@gmail.com>
2024-11-24 15:57:24 -0300

    organização de rotas

--- server/src/routes/routes.ts
+++ server/src/routes/routes.ts
@@ -3,6 +3,7 @@ import authRoutes from "./authRoutes";
 import resetPasswordRoutes from "./resetPasswordRoutes";
 import transactionRoutes from "./transactionRoutes";
 import priceRoutes from "./priceRoutes";
+import userRoutes from "./userRoutes";
 import { errorHandler } from "../middleware/errorHandler";
 
 const router = Router();
@@ -11,7 +12,7 @@ router.use(authRoutes);
 router.use(resetPasswordRoutes);
 router.use(transactionRoutes);
 router.use(priceRoutes);
-
 router.use(errorHandler);
+router.use(userRoutes);
 
 export default router;
