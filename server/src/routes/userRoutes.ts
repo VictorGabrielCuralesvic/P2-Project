f5b2e933dc1bdbde6ac7c387e06fb1d7e2df4ab3
elise <Elisenovaes10@gmail.com>
2024-12-02 08:29:07 -0300

    Update userRoutes.ts

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -8,4 +8,7 @@ const userController = new UserController();
 // Rota para atualizar o perfil do usuário
 router.put("/user", authMiddleware, userController.updateUser);
 
+// Rota para buscar informações do usuário
+router.get("/user", authMiddleware, userController.getUserInfo);
+
 export default router;
